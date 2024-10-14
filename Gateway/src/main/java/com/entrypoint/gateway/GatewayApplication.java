73447aa210fe7bfed9fcbb6080de9676094b1af7
Muszchrom <altair2468@o2.pl>
2024-10-14 21:30:43 +0200

    disabled default spring security config

--- Gateway/src/main/java/com/entrypoint/gateway/GatewayApplication.java
+++ Gateway/src/main/java/com/entrypoint/gateway/GatewayApplication.java
@@ -2,8 +2,11 @@ package com.entrypoint.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
-@SpringBootApplication
+@EnableR2dbcRepositories
+@SpringBootApplication(exclude={ReactiveSecurityAutoConfiguration.class})
 public class GatewayApplication {
 
 	
