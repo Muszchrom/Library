0badea73eaef87548a9758ee21010ceefcb2457e
MaQlu <b.bartek450@interia.pl>
2024-10-19 11:58:13 +0200

    added auth filters for admin,employee and users

--- Gateway/src/main/java/com/entrypoint/gateway/GatewayApplication.java
+++ Gateway/src/main/java/com/entrypoint/gateway/GatewayApplication.java
@@ -1,5 +1,6 @@
 package com.entrypoint.gateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
@@ -9,7 +10,17 @@ import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 @SpringBootApplication(exclude={ReactiveSecurityAutoConfiguration.class})
 public class GatewayApplication {
 
-	
+	@Autowired
+	private AuthFilter authFilter;
+
+	@Autowired
+	private AuthFilterAdmin authFilterAdmin;
+
+	@Autowired
+	private AuthFilterEmployee authFilterEmployee;
+
+	@Autowired
+	private AuthFilterUser authFilterUser;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
