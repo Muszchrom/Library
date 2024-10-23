5821c54cb7a838887c8337738857f1a87f3f317e
Matijasek <anatol01@o2.pl>
2024-10-23 16:49:48 +0200

    added setting for connecting to the database

--- Backend/app/settings.py
+++ Backend/app/settings.py
@@ -74,11 +74,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',   
+        'NAME': 'student',                           
+        'USER': 'student',                           
+        'PASSWORD': 'student',                       
+        'HOST': 'localhost',                         
+        'PORT': '5432',                             
     }
 }
 
