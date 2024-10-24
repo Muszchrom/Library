17a049e48b70350ddea47b8e066a51bf4b79dda0
Matijasek <anatol01@o2.pl>
2024-10-24 11:43:50 +0200

    updated settings

--- Backend/app/settings.py
+++ Backend/app/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',   
-        'NAME': 'student',                           
+        'NAME': 'books_database',                           
         'USER': 'student',                           
         'PASSWORD': 'student',                       
-        'HOST': 'localhost',                         
+        'HOST': 'database',                         
         'PORT': '5432',                             
     }
 }
