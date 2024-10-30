f88357033d36a93f8d06f10092f19de153616132
MaQlu <b.bartek450@interia.pl>
2024-10-31 00:05:58 +0100

    route mods

--- Gateway/src/main/java/com/entrypoint/gateway/GatewayApplication.java
+++ Gateway/src/main/java/com/entrypoint/gateway/GatewayApplication.java
@@ -179,7 +179,7 @@ public class GatewayApplication {
 						.uri("http://backend:8000")
 				)
 
-				//jebanygit
+				//jebanygitKURWA
 				//allow everything for everyone
 //				.route("GET from all" , p-> p
 //						.path("/waz/**")
