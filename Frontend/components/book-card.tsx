d863610458197b0875493b3826452a9a1f6c3fcc
Muszchrom <altair2468@o2.pl>
2024-11-13 21:43:27 +0100

    Nicer images

--- Frontend/components/book-card.tsx
+++ Frontend/components/book-card.tsx
@@ -3,16 +3,33 @@ import Image from "next/image";
 import Score from "./score";
 import { Book } from "@/interfaces";
 
-export default function BookCard({bookData}: {bookData: Book}) {
+export default async function BookCard({bookData}: {bookData: Book}) {
   const baseUrl = "/book";
-  const temp_cover = "https://static01.helion.com.pl/global/okladki/vbig/arcapi.jpg"
+  const coverUrl = await (async () => {
+    const coverNotFound = process.env.GATEWAY_URL + "waz/media/covers/default_cover.jpg"
+
+    const x = bookData.cover_book.split(":")[2]; // split into 3 parts, http, domain, path
+    const url = process.env.GATEWAY_URL + "waz"  + x.substring(x.indexOf("/")); // remove port
+    const y = await fetch(url, {method: "HEAD"})
+    return y.status == 200 ? url : coverNotFound
+  })()
   {/*bookData.coverURL  */} 
   // 176 - 240
   return (
     <div className="w-full h-full flex flex-col">
-      <Link href={baseUrl + "/" + bookData.id}>
-        <Image className="rounded-md w-full" alt="" height={100} width={176} src={bookData.cover_book} />
-      </Link>
+      <div className="w-40 h-56 overflow-hidden border-md">
+        <Link href={baseUrl + "/" + bookData.id} className="w-full h-full flex items-center justify-center relative ">
+          <Image 
+            className="rounded-md w-full h-full object-contain relative z-10" 
+            alt="" 
+            height={224} 
+            width={160} 
+            src={coverUrl} />
+          <div className="w-full h-full object-cover absolute top-0 bottom-0 z-0 rounded-md overflow-hidden">
+            <Image className="w-auto h-auto object-cover blur-md" alt="" fill={true} quality={1} sizes="1vw" src={coverUrl} />
+          </div>
+        </Link>
+      </div>
       <h3 className="font-medium leading-none tracking-tight px-1 pt-2 break-words">
         {bookData.title}
       </h3>
