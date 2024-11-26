16064ead8d584f7c69746dcec45921255ddba5f7
Muszchrom <altair2468@o2.pl>
2024-11-26 03:18:53 +0100

    Only employees and above can see books internal navbar

--- Frontend/app/(site)/books/layout.tsx
+++ Frontend/app/(site)/books/layout.tsx
@@ -1,18 +1,30 @@
 import ButtonLink from "@/components/button-link";
 import { Separator } from "@/components/ui/separator";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
 import React from "react";
 
-export default function BookLayout({children}: {children: React.ReactNode}) {
-  return (
-    <div className="flex flex-col gap-3">
-      <div className="flex w-full justify-between gap-2 sm:gap-4">
-        <ButtonLink to="/books/browse" emoji="🔎">Przeglądaj</ButtonLink>
-        <ButtonLink to="/books/add" emoji="➕">Dodaj</ButtonLink>
-        <ButtonLink to="/books/stats" emoji="📈">Statystyki</ButtonLink>
+export default async function BookLayout({children}: {children: React.ReactNode}) {
+  const session = await getServerSession(authOptions);
+
+  if (session?.user.role == "2" || session?.user.role == "1") {
+    return (
+      <div className="flex flex-col gap-3">
+        <div className="flex w-full justify-between gap-2 sm:gap-4">
+          <ButtonLink to="/books/browse" emoji="🔎">Przeglądaj</ButtonLink>
+          <ButtonLink to="/books/add" emoji="➕">Dodaj</ButtonLink>
+          <ButtonLink to="/books/stats" emoji="📈">Statystyki</ButtonLink>
+        </div>
+        <Separator orientation="horizontal"/>
+        {children}
       </div>
-      <Separator orientation="horizontal"/>
-      {children}
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div className="flex flex-col gap-3">
+        {children}
+      </div>
+    )
+  }
 }
 
